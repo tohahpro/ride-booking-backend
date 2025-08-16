62dea88141fbdd907ea035826e4909a725c36982
Toha Hossain <tohahpro@gmail.com>
2025-08-16 13:58:37 +0600

    user logout setup

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -10,40 +10,65 @@ import { setAuthCookie } from '../../utils/Jwt/setCookie';
 import passport from 'passport';
 
 
-const credentialsLogin = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
+const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     // const loginInfo = await AuthService.credentialsLogin(req.body);
 
-    passport.authenticate("local",async(err: any, user: any, info: any)=>{
-        
-        if(err){
+    passport.authenticate("local", async (err: any, user: any, info: any) => {
+
+        if (err) {
             return next(new AppError(401, err))
         }
 
-        if(!user){
+        if (!user) {
             return next(new AppError(401, info.message))
         }
 
         const userTokens = await createUserToken(user)
-        
-        const {password: pass, ...rest} = user.toObject()
+
+        const { password: pass, ...rest } = user.toObject()
 
         setAuthCookie(res, userTokens)
 
-        sendResponse(res,{
+        sendResponse(res, {
             success: true,
             statusCode: httpStatus.OK,
             message: 'User Logged In successfully',
-            data : {
-                accessToken : userTokens.accessToken,
-                refreshToken : userTokens.refreshToken,
-                user : rest
+            data: {
+                accessToken: userTokens.accessToken,
+                refreshToken: userTokens.refreshToken,
+                user: rest
             }
         })
-    })(req,res,next)
+    })(req, res, next)
 
-    
+
 });
 
-export const AuthControllers ={
-    credentialsLogin
+
+const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    res.clearCookie("accessToken", {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    })
+
+    res.clearCookie("refreshToken", {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    })
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'User logged out successfully',
+        data: null
+    })
+
+})
+
+export const AuthControllers = {
+    credentialsLogin,
+    logout,
+    
 }
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,5 +5,6 @@ import { AuthControllers } from "./auth.controller";
 const router = Router()
 
 router.post('/login', AuthControllers.credentialsLogin)
+router.post('/logout', AuthControllers.logout)
 
 export const AuthRoutes = router;
