8734312904da6c5b38eef2f043cc9e3506d38d91
Toha Hossain <tohahpro@gmail.com>
2025-08-16 12:29:01 +0600

    create New Access Token With Refresh Token

--- src/app/utils/Jwt/user.token.ts
+++ src/app/utils/Jwt/user.token.ts
@@ -1,6 +1,10 @@
-import { IUser } from "../../modules/user/user.interface";
-import { generateToken } from './jwt';
+import httpStatus from 'http-status-codes';
+import { IsActive, IUser } from "../../modules/user/user.interface";
+import { generateToken, verifyToken } from './jwt';
 import { envVars } from '../../config/env';
+import AppError from "../../errorHandlers/AppError";
+import { User } from "../../modules/user/user.model";
+import { JwtPayload } from "jsonwebtoken";
 
 
 export const createUserToken = (user: Partial<IUser>)=>{
@@ -19,4 +23,31 @@ export const createUserToken = (user: Partial<IUser>)=>{
         accessToken,
         refreshToken
     }
+};
+
+
+
+
+export const createNewAccessTokenWithRefreshToken = async(refreshToken: string)=>{
+    const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload
+    
+    const isUserExist = await User.findOne({email: verifiedRefreshToken.email})
+
+    if(!isUserExist){
+        throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist")
+    }
+    if(isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE){
+        throw new AppError(httpStatus.BAD_REQUEST, `user is ${isUserExist.isActive}`)
+    }
+
+
+    const jwtPayload = {
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    
+    return accessToken
+    
 }
